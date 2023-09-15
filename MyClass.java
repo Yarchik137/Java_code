fb119e1f113a6f0b3a1dd0d6b879924980147978
Yarchik137 <145118828+Yarchik137@users.noreply.github.com>
2023-09-15 14:28:03 +0300

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,12 @@
+import static java.lang.System.*;
+
+public class MyClass {
+    public static  void main(String[] args) {
+        System.out.println("hi zip");
+        System.out.println("hi zip");
+        System.out.print("hi zip");
+
+
+    }
+
+}
